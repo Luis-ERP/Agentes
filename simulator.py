a87311e8656c58a2a917fb06cb700356f745cce7
Edgar Ramirez <leramirezp@hotmail.com>
2023-06-14 14:42:32 -0500

    .

--- simulator.py
+++ simulator.py
@@ -7,6 +7,20 @@ import matplotlib.colors as mcolors
 import random
 import numpy as np
 
+
+def get_random_element(numbers, key_func):
+    # Calculate the weights for each number
+    weights = [1 / key_func(num) if key_func(num) != 0 else 1 for num in numbers]
+    # Normalize the weights to sum up to 1
+    total_weight = sum(weights)
+    normalized_weights = [weight / total_weight for weight in weights]
+    indices = range(len(numbers))
+    # Use random.choices to select a random element based on the weights
+    random_element = random.choices(indices, weights=normalized_weights)[0]
+    
+    return numbers[random_element]
+
+
 class VictimAgent(Agent):
     def __init__(self, model):
         super().__init__(random.random(), model)
@@ -55,7 +69,7 @@ class RescueAgent(Agent):
         victim = self.is_victim_at_current_position()
         
         if self.mode == "searching":
-            y, x = self.pos
+            x, y = self.pos
             self.model.cells[y][x] += 1
 
             if victim:
@@ -65,26 +79,26 @@ class RescueAgent(Agent):
                 close_victims = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False,
                                                       radius=self.visibility_radio)
                 close_victims = list(filter(lambda agent: agent.type=="victim" and agent.follow==None, close_victims))
+
                 if close_victims:
                     cells = [agent.pos for agent in close_victims] 
                 else:
                     cells = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
-                try:
-                    cells_heat_map = [self.model.cells[n][m] for n, m in cells ]
-                except Exception as e:
-                    print(cells)
-                    raise e
+                
+                # Get the most visited cells and pick a random weighted from from least visite to most
+                cells_heat_map = [ self.model.cells[n][m] for m, n in cells ]
                 cell_choices = list(zip(cells_heat_map, cells))
                 cell_choices.sort(key=lambda c: c[0])
-                new_position = cell_choices[0][1]
+                new_position = get_random_element(cell_choices, lambda c: c[0])[1]
+
                 self.model.grid.move_agent(self, new_position)
 
         elif self.mode == "guiding" and not self.in_exit():
             new_x, new_y = self.pos
             if self.pos[0] < self.model.WIDTH-1: # X
                 new_x += 1
-            if self.pos[1] > 0: # Y
-                new_y -= 1
+            if self.pos[1] < self.model.HEIGHT-1: # Y
+                new_y += 1
             self.model.grid.move_agent(self, (new_x, new_y))
             
         elif self.mode == "guiding" and self.in_exit():
@@ -92,14 +106,15 @@ class RescueAgent(Agent):
     
 
 class Warehouse(Model):
-    WIDTH, HEIGHT = 30, 50
-    EXIT_CELLS = [(28,0), (29,0)] # [(4,0), (3,0)]
-    RESCUERS = 10
-    VICTIMS = 50
+    WIDTH, HEIGHT = 10, 10 #30, 50
+    EXIT_CELLS =  [(WIDTH-2, HEIGHT-1), (WIDTH-1, HEIGHT-1)]
+    RESCUERS = 3
+    VICTIMS = 10
 
     def __init__(self):
         self.grid = MultiGrid(self.WIDTH, self.HEIGHT, torus=False)
-        self.cells = np.zeros((self.WIDTH, self.HEIGHT))
+        self.cells = np.zeros((self.HEIGHT, self.WIDTH))
+        self.cells[0][0] = 1
         self.schedule = RandomActivation(self)
 
         # Place victim agents
@@ -131,18 +146,20 @@ class Warehouse(Model):
         return serialized_agents
     
     def convert_to_plot_matrix(self):
-        from pprint import pprint
-        matrix = [[0 for m in range(self.HEIGHT)] for n in range(self.WIDTH)]
-        for m, n in self.EXIT_CELLS:
-            matrix[n][m] = -1
-
+        matrix = [[0 for m in range(self.WIDTH)] for n in range(self.HEIGHT)]
+        # Color the historical steps
         for n in range(len(self.cells)):
             for m in range(len(self.cells[n])):
                 matrix[n][m] = self.cells[n][m]
 
+        # Color the exit points
+        for m, n in self.EXIT_CELLS:
+            matrix[n][m] = -1
+        
+        # Color the agents
         for agents, m, n in self.grid.coord_iter():
             for agent in agents:
-                y, x = agent.pos
+                x, y = agent.pos
                 if agent.type == 'victim':
                     matrix[y][x] = -2
                 elif agent.type == 'rescuer':
@@ -158,21 +175,23 @@ class Warehouse(Model):
         # Define colormap
         colors = ['gray', 'black', 'blue', 'red', 'white']
         green_colormap = mpl.colormaps['Greens']
-
         cmap_list = []
         # Add colors for values -4 to 0
         for color in colors:
             cmap_list.append(mcolors.to_rgba(color))
         
         # Add colors for values 1 to 255 using the sequential greens colormap
-        for i in range(1, 256):
+        bounds = [-4.5, -3.5, -2.5, -1.5, -0.5, 0.5]
+        counter = 1
+        for i in range(1, 256, 25):
             rgba = green_colormap(i)
             cmap_list.append((rgba[0], rgba[1], rgba[2], rgba[3]))
+            bounds.append(counter * 1.5)
+            counter += 1
         
         custom_cmap = mcolors.ListedColormap(cmap_list)
-        bounds = [-4.5, -3.5, -2.5, -1.5, -0.5, 0.5] + [1.5*c for c in range(6, len(cmap_list))]
-        norm = mcolors.BoundaryNorm(bounds, custom_cmap.N)
-        
+        norm = plt.cm.colors.BoundaryNorm(bounds, custom_cmap.N)
+
         # Plot the grid
         plt.imshow(matrix, cmap=custom_cmap, norm=norm, interpolation='none')
         plt.grid(True, which='both', color='black', linewidth=0.5)
